e6b5ddadad87c4767310ae9e1ae85148dddf18b4
Kyoung DoHyun <rudehgus1@naver.com>
2020-06-21 15:06:09 +0900

    설명추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Score_Management import Score_Management_System
 
 if __name__ == "__main__":
     sms = Score_Management_System()
-    sms.read("C:\\working\\openSource_lecture\\final_project\\gwnu_score_management_system\\score.csv")
+    sms.read("C:\\working\\openSource_lecture\\final_project\\gwnu_score_management_system\\score.csv")#경로 오류로 인하여 절대경로로 지정
     print(sms.sort("avg", "des"))
     sms.write("C:\\working\\openSource_lecture\\final_project\\gwnu_score_management_system\\result.csv", "avg", "des")
 #경로 오류로 인하여 절대경로로 지정
